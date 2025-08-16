da9108cec449a6c5d257bce26a4df0ae7f5b50b5
rafayD <rafaydin@gmail.com>
2025-08-16 19:32:25 -0400

    remove arrow down

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -69,10 +69,6 @@ export function HeroSection() {
             </div>
           </div>
         </div>
-
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <ArrowDown className="w-6 h-6 text-muted-foreground" />
-        </div>
       </div>
     </section>
   )
